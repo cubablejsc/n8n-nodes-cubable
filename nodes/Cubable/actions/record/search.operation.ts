9c0ed4b1a4ababe2984b158ca0d0923f5b3da96c
Qui Nguyen <qui.nguyen@cubable.com>
2024-12-26 10:48:07 +0700

    chore: cubable node

--- nodes/Cubable/actions/record/search.operation.ts
+++ nodes/Cubable/actions/record/search.operation.ts
@@ -27,32 +27,58 @@ export const description: INodeProperties[] = updateDisplayOptions(
 	properties
 );
 
+async function query(
+	this: IExecuteFunctions,
+	qs: IDataObject,
+	_page: number = 1,
+	_arr: any[] = []
+) {
+	qs.page = _page;
+
+	const response = await apiRequest.call( this, 'GET', 'records', qs );
+	const data = response.data || [];
+
+	_arr.push( ...data );
+
+	if ( data.length === qs.pageSize ) {
+		qs.sessionID = response.sessionID;
+
+		// @ts-ignore
+		await new Promise( resolve => setTimeout( resolve, 500 ) );
+		await query.call( this, qs, _page + 1, _arr );
+	}
+
+	return _arr;
+}
+
 export async function execute(
 	this: IExecuteFunctions,
 	items: INodeExecutionData[],
 	baseID: string,
 	tableID: string,
 ): Promise<INodeExecutionData[]> {
-	const viewID: string = this.getNodeParameter( 'view', 0, undefined, {
-		extractValue: true,
-	} ) as string;
 	const returnFieldsByFieldID: boolean
 		= this.getNodeParameter( 'returnFieldsByFieldID', 0 ) as boolean;
 	const qs: IDataObject = {
 		baseID,
 		tableID,
 		returnFieldsByFieldID,
+		pageSize: 50,
 	};
+
+	const viewID: string = this.getNodeParameter( 'view', 0, undefined, {
+		extractValue: true,
+	} ) as string;
 
 	if ( viewID ) qs.viewID = viewID;
 
-	const response = await apiRequest.call( this, 'GET', 'records', qs );
+	const records = await query.call( this, qs );
 
 	const expandCustomFields: boolean
 		= this.getNodeParameter( 'expandCustomFields', 0 ) as boolean;
 	const returnData: INodeExecutionData[] = [];
 
-	for ( const record of response.data || [] ) {
+	for ( const record of records ) {
 		let json: any;
 
 		if ( expandCustomFields ) {
